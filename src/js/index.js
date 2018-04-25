174320a1a054975191e0172e65fdd23d63499dcd
frankievalentine <frankievalentine9@gmail.com>
2018-04-25 12:32:51 -0700

    Coded initial global state and search control functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,33 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
+// Global state
+// - Search object
+// - Current recipe object
+// - Shopping list object
+// - Liked recipes
+const state = {};
+
+const controlSearch = async () => {
+    // Get query from view
+    const query = 'pizza'; // todo
+
+    if (query) {
+        // New search object and add to state
+        state.search = new Search(query);
+        // Prepare UI for results
+
+        // Await for recipes on new Search object
+        await state.search.getResults();
+
+        // Render results on UI after we get it from API
+        console.log(state.search.result);
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
 
 // http://food2fork.com/api/search
