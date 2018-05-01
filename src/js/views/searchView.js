e7d9b8319f740dd46cdef7544e10486d29300f88
frankievalentine <frankievalentine9@gmail.com>
2018-05-01 10:25:54 -0700

    Used fractional.js for formatting rational nums on recipeView model

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -12,6 +12,16 @@ export const clearResults = () => {
     elements.searchResPages.innerHTML = '';
 };
 
+// Highlight selected recipe
+export const highlightSelected = id => {
+    const resultsArr = Array.from(document.querySelectorAll('.results__link'));
+    resultsArr.forEach(el => {
+        el.classList.remove('results__link--active');
+    });
+
+    document.querySelector(`a[href="#${id}"]`).classList.add('results__link--active');
+};
+
 // 'Pasta with tomato and spinach' = ['Pasta', 'with', 'tomato, 'and', 'spinach']
 // acc = 0 / acc + cur.length = 5 newTitle = ['Pasta']
 // acc = 5 / acc + cur.length = 9 newTitle = ['Pasta', 'with']
