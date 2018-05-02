a4fb8b550d5814133f1455d66cebc624f32ed422
frankievalentine <frankievalentine9@gmail.com>
2018-05-02 16:54:33 -0700

    Built the likes model with add, delete, isLiked, getNumLikes methods

--- src/js/views/listView.js
+++ src/js/views/listView.js
@@ -15,10 +15,10 @@ export const renderItem = item => {
             </button>
         </li>
     `;
-    element.shopping.insertAdjacentHTML('beforeend', markup);
+    elements.shopping.insertAdjacentHTML('beforeend', markup);
 };
 
 export const deleteItem = id => {
     const item = document.querySelector(`[data-itemid="${id}"]`);
-    item.parentElement.removeChild(item);
+    if (item) item.parentElement.removeChild(item);
 };
